ca193a41edc7ade2d5d1d494f4dfb79fd72141a8
Leonardo Zotto <leonardo_dusk2@hotmail.com>
2020-04-30 07:17:29 -0300

    Search front-end

--- app/src/main/java/com/example/mangaq/activity/fragment/PesquisaFragment.java
+++ app/src/main/java/com/example/mangaq/activity/fragment/PesquisaFragment.java
@@ -1,26 +1,34 @@
 package com.example.mangaq.activity.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ProgressBar;
+
+import androidx.fragment.app.Fragment;
+
 import com.example.mangaq.R;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class PesquisaFragment extends Fragment {
 
+    private ProgressBar progressBar;
+    private CircleImageView imageView;
+    public GridView gridViewPerfil;
 
     public PesquisaFragment() {
-    }
 
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_pesquisa, container, false);
     }
+
 
 }
